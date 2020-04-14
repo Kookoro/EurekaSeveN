d1eff84880cad66c22873b5b93b28128916de2ec
qiulin <710937671@qq.com>
2020-04-14 17:29:04 +0800

    2020041417:28

--- reactdemo/src/components/Article.js
+++ reactdemo/src/components/Article.js
@@ -1,8 +1,9 @@
 import React, { Component, useState } from "react";
 import { Link, useHistory } from "react-router-dom";
-import { Button, Input } from "antd";
+import { Button, Input, Alert } from "antd";
 
 const Article = () => {
+  const [isNull, setIsNull] = useState(false);
   const [state, setstate] = useState({
     list: [
       {
@@ -33,15 +34,34 @@ const Article = () => {
   const onInputChange = (e) => {
     const inputValue = e.target.value,
       inputName = e.target.name;
+    //Object.assign() 方法用于将所有可枚举属性的值从一个或多个源对象复制到目标对象。它将返回目标对象。
+    /*
+
+            const target = { a: 1, b: 2 };
+            const source = { b: 4, c: 5 };
+
+            const returnedTarget = Object.assign(target, source);
+
+            console.log(target);
+            // expected output: Object { a: 1, b: 4, c: 5 }
+
+            console.log(returnedTarget);
+            // expected output: Object { a: 1, b: 4, c: 5 }
+
+       */
     const data = Object.assign({}, newArticle, { [inputName]: inputValue });
     setNewArticle(data);
   };
   const addArticle = () => {
+    if (!newArticle.aid || !newArticle.content || !newArticle.title) {
+      return setIsNull(true);
+    }
     let newList = state.list;
     newList.push(newArticle);
     setstate({
       list: newList,
     });
+    setIsNull(false);
   };
   const deleteComment = (e) => {
     let undeleteList = state.list;
@@ -55,6 +75,13 @@ const Article = () => {
     <div>
       评论组件
       <ul>
+        {/* Keys 是 React 用于追踪哪些列表中元素被修改、被添加或者被移除的辅助标识。
+        
+        1.在开发过程中，我们需要保证某个元素的 key 在其同级元素中具有唯一性。
+        2.在 React Diff 算法中 React 会借助元素的 Key 值来判断该元素是新近创建的还是被移动而来的元素，从而减少不必要的元素重渲染。
+        3.此外，React 还需要借助 Key 值来判断元素与本地状态的关联关系，因此我们绝不可忽视转换函数中 Key 的重要性。
+        
+        */}
         {state.list.map((value, key) => {
           return (
             <li key={key}>
@@ -78,6 +105,9 @@ const Article = () => {
           );
         })}
       </ul>
+      {isNull ? (
+        <Alert message="Error" type="error" description="请填写内容！"></Alert>
+      ) : null}
       <Input
         placeholder="请填写标题"
         name="title"

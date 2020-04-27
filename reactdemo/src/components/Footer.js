c491d2c3e78796e17522e23af2ed57795d5eae7c
qiulin <710937671@qq.com>
2020-04-27 21:28:30 +0800

    20200427

--- reactdemo/src/components/Footer.js
+++ reactdemo/src/components/Footer.js
@@ -1,12 +1,22 @@
-import React, { useState } from "react";
-import { Input } from "antd";
+import React, { useState, useEffect } from "react";
+import { Input, Button } from "antd";
 const Footer = () => {
   const [state, setstate] = useState({
     value: "",
   });
+  const [arr, setArr] = useState([]);
+  const [count, setCount] = useState(0);
+  // Similar to componentDidMount and componentDidUpdate:
+  // 类似于 componentDidMount 和 ComponentDidUpdate
+  useEffect(() => {
+    // Update the document title using the browser API
+
+    const box1 = document.querySelector("#box1");
+    debugger;
+  });
   return (
     <div>
-      <div>react 双向数据绑定</div>
+      <div> react 双向数据绑定 </div>{" "}
       <Input
         placeholder="请输入"
         onChange={(e) => {
@@ -14,8 +24,17 @@ const Footer = () => {
             value: e.target.value,
           });
         }}
-      />
-      <h1>{state.value}</h1>
+      />{" "}
+      <Button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        {" "}
+        点击{" "}
+      </Button>
+      <h1> {state.value} </h1> <h1> 你点击了{count}次 </h1>
+      <div id="box1"></div>
     </div>
   );
 };

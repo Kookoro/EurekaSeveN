3432ec64496e018a860033568093ffc939860777
qiulin <710937671@qq.com>
2020-06-02 18:06:44 +0800

    20200602

--- reactdemo/src/components/childrenComponent/leetcode.jsx
+++ reactdemo/src/components/childrenComponent/leetcode.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button } from "antd";
+import { Button, Input } from "antd";
 class LeetCode extends React.Component {
   constructor(props) {
     super(props);
@@ -7,10 +7,12 @@ class LeetCode extends React.Component {
       candies: [2, 3, 5, 1, 3],
       key: 3,
       result: [],
+      value: 0,
     };
     this.kidsWithCandies = this.kidsWithCandies.bind(this);
   }
   kidsWithCandies(candies, extraCandies) {
+    //leetcode  https://leetcode-cn.com/problems/qiu-12n-lcof/
     let result2 = [],
       status = false;
     for (let i = 0; i < candies.length; i++) {
@@ -28,6 +30,9 @@ class LeetCode extends React.Component {
       result: result2,
     });
   }
+  accumulation(n) {
+    return n && n + this.accumulation(--n); //累加算法
+  }
   render() {
     return (
       <div>
@@ -40,6 +45,27 @@ class LeetCode extends React.Component {
           点击
         </Button>
         <h2>1{this.state.result.toString()}</h2>
+        <div>
+          <a href="https://leetcode-cn.com/problems/qiu-12n-lcof/">累加算法</a>
+
+          <Input
+            autoComplete="off"
+            placeholder="input a number"
+            name="aid"
+            onChange={(e) => {
+              const value = e.target.value;
+              this.setState({
+                value: this.accumulation(parseFloat(value))
+                  ? this.accumulation(parseFloat(value))
+                  : 0,
+              });
+            }}
+          ></Input>
+          <h1>
+            {" "}
+            <span>result:{this.state.value}</span>
+          </h1>
+        </div>
       </div>
     );
   }

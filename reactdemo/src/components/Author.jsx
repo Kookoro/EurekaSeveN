139749bf00ab077125962b6bb19907ad25e52b55
qiulin <710937671@qq.com>
2020-06-08 11:28:34 +0800

    20200608

--- reactdemo/src/components/Author.jsx
+++ reactdemo/src/components/Author.jsx
@@ -10,6 +10,19 @@ let ob = observable({
 class Auther extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      testObject: {
+        a: "1",
+      },
+      testArr: ["1"],
+      testRegExp: /runoob/i,
+      testBoolean: false,
+      testSymbol: new Symbol("PI"),
+      testString: "SSSSSS",
+      testFunc: () => {
+        console.log(this.state.testFunc);
+      },
+    };
   }
   changeObValue() {
     ob.a++;
@@ -19,12 +32,20 @@ class Auther extends Component {
       console.log("autorun监听", ob.a);
     });
   }
-
+  printType(obj) {
+    return Object.prototype.toString.call(obj);
+  }
   render() {
     const store = appStore;
     return (
       <div>
         Auther <h1> {store.timer} </h1>
+        <h1> {this.printType(this.state.testArr)} </h1>
+        <h1> {this.printType(this.state.testRegExp)} </h1>
+        <h1> {this.printType(this.state.testBoolean)} </h1>
+        <h1> {this.printType(this.state.testSymbol)} </h1>
+        <h1> {this.printType(this.state.testString)} </h1>
+        <h1> {this.printType(this.state.testFunc)} </h1>
         <AsyncText></AsyncText>
         <Button onClick={this.changeObValue}> click </Button>{" "}
       </div>

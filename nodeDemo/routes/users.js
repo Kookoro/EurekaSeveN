b697fc7497cc1c8cb1d649d3edc7bc285684737f
qiulin <710937671@qq.com>
2020-06-28 11:11:53 +0800

    express api learning

--- nodeDemo/routes/users.js
+++ nodeDemo/routes/users.js
@@ -2,8 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/helloworld', function (req, res, next) {
+  res.json({
+    mes: 'helloworld11！',
+    status: true,
+    username: 'linl1n',
+    usercode: '00001',
+    isTrue: false,
+  })
+  res.send('helloworld!');
+});
+
 
 module.exports = router;
--- reactdemo/src/components/Clock.jsx
+++ reactdemo/src/components/Clock.jsx
@@ -1,6 +1,44 @@
 import React from "react";
 import axios from "axios";
 import "../css/clock.css";
+import { Button } from "antd";
+class StdClock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nowDate: "",
+    };
+    // this.cancelTimer = this.cancelTimer.bind(this);
+  }
+  tick() {
+    this.getBeijingTime();
+  }
+  getBeijingTime() {
+    axios.get("http://quan.suning.com/getSysTime.do").then((res) => {
+      this.setState({
+        nowDate: res.data.sysTime2,
+      });
+    });
+  }
+  render() {
+    return <div>当前北京时间:{this.state.nowDate}</div>;
+  }
+  cancelTimer() {
+    clearInterval(this.timerID);
+  }
+  startTimer() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+  componentDidMount() {
+    //会在组件已经被渲染到dom中后执行
+    this.props.onRef(this);
+    this.startTimer();
+  }
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+    this.getBeijingTime = null;
+  }
+}
 class Clock extends React.Component {
   constructor(props) {
     super(props);
@@ -10,10 +48,13 @@ class Clock extends React.Component {
       datas: this.props.location.state,
       nowDate: "",
     };
+    this.getFirstNodeApi = this.getFirstNodeApi.bind(this);
+    this.pauseTimer = this.pauseTimer.bind(this);
+    this.startTimer = this.startTimer.bind(this);
   }
   componentDidMount() {
     //会在组件已经被渲染到dom中后执行
-    this.timerID = setInterval(() => this.tick(), 1000);
+    // this.timerID = setInterval(() => this.tick(), 1000);
   }
   componentWillUnmount() {
     //组件dom卸载前
@@ -39,6 +80,17 @@ class Clock extends React.Component {
         nowDate: res.data.sysTime2,
       });
     });
+  }
+  getFirstNodeApi() {
+    axios.get("http://localhost:3080/users/helloworld").then((res) => {
+      console.log(res);
+    });
+  }
+  pauseTimer() {
+    this.stdClock.cancelTimer();
+  }
+  startTimer() {
+    this.stdClock.startTimer();
   }
   render() {
     return (
@@ -67,9 +119,16 @@ class Clock extends React.Component {
           用户姓名:{this.state.datas.name}
           用户年龄:{this.state.datas.age}
           登录日期:{this.state.datas.year}
-          当前北京时间:{this.state.nowDate}
+          <StdClock
+            onRef={(ref) => {
+              this.stdClock = ref;
+            }}
+          ></StdClock>
         </h1>
         <h2>it is {this.state.date.toLocaleTimeString()}</h2>
+        <Button onClick={this.pauseTimer}>暂停时钟</Button>
+        <Button onClick={this.startTimer}>启用时钟</Button>
+        <Button onClick={this.getFirstNodeApi}>点击请求</Button>
       </div>
     );
   }

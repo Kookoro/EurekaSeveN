fc61f974486bf27d2afd4f4e9cbc4b271490c23f
qiulin <710937671@qq.com>
2020-07-10 16:48:11 +0800

    20200710

--- reactdemo/src/components/childrenComponent/page/asyncAwait.jsx
+++ reactdemo/src/components/childrenComponent/page/asyncAwait.jsx
@@ -1,4 +1,14 @@
 /**
+ * ES2017 -async
+ * Generator函数的语法糖
+ * 相较于function *  和yield async表示函数中带有异步操作，await则表示紧跟在其后的表达式需要等待结果
+ * 
+ * 内置执行器 
+ * 语义化更好
+ * 适用性更广
+ * 返回Promise对象
+ * 
+ * 
  * async await异步编程终极解决方案  对比promise .then
  *
  * async ====“异步”
@@ -20,6 +30,7 @@ class AsyncText extends React.Component {
       val: "hello async!",
     };
   }
+
   render() {
     const result = this.asyncLearn();
     // async 函数返回的是一个 Promise 对象，所以在最外层不能用 await 获取其返回值的情况下，我们当然应该用原来的方式：then() 链来处理这个 Promise 对象，就像这样、
@@ -32,6 +43,7 @@ class AsyncText extends React.Component {
   async asyncLearn() {
     return this.state.val;
   }
+  async getStockPriceByName() {}
 }
 export default AsyncText;
 /**

7ee48f5b771520f13920b596298838a15704fd63
qiulin <710937671@qq.com>
2020-07-16 14:19:10 +0800

    20200716 更新index.js

--- nodeDemo/routes/index.js
+++ nodeDemo/routes/index.js
@@ -1,10 +1,15 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', {
-    title: 'Express'
+router.get("/", function (req, res, next) {
+  res.render("index", {
+    title: "Express",
+  });
+});
+router.get("/users", function (req, res, next) {
+  res.render("users", {
+    title: "Express",
   });
 });
 

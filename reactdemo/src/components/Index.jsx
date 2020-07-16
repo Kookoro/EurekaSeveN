f3c785044e3c8bcf08f9bd6d41f9b8c0e47712c6
qiulin <710937671@qq.com>
2020-07-16 22:55:01 +0800

    20200716

--- reactdemo/src/components/Index.jsx
+++ reactdemo/src/components/Index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 // import { Button } from "antd";
 // import { Link, BrowserRouter as Router } from "react-router-dom";
 // import ChildMenu from "../components/childrenComponent/ChildMenu";

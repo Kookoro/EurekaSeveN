c5ca5f6533427bfbe3d6f87c54ebe9aaf3243d53
邱麟 <710937671@qq.com>
2020-07-21 21:36:03 +0800

    20200721

--- reactdemo/src/components/childrenComponent/page/reacthooks/reacthooksLearn.jsx
+++ reactdemo/src/components/childrenComponent/page/reacthooks/reacthooksLearn.jsx
@@ -1,5 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect,createContext,useContext } from "react";
 import { Button } from "antd";
+
+const CountContext = createContext()
+const Counter = ()=>{
+    const context = useContext(CountContext)
+    return(
+        <div>
+            <h1>childCount: {context}</h1>
+        </div>
+    )
+}
+
 const Demo = () => {
   const [count, setCount] = useState(0);
   const [weight, setWeight] = useState(100);
@@ -15,10 +26,20 @@ const Demo = () => {
   useEffect(() => {
     console.log("default update lifecycle");
   }, );//
+
+  //useContext解决父子组件传值  与useReducer区别
+
+
+
+
+
   return (
     <div>
       <h1>count:{count}</h1>
       <h1>weight:{weight}</h1>
+      <CountContext.Provider value={count}>
+          <Counter></Counter>
+      </CountContext.Provider>
       <Button
         onClick={() => {
           setCount(count + 1);

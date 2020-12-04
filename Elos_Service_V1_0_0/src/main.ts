e15401f90cda2ee9c6e311649a2fa230de59eb55
qiulin <710937671@qq.com>
2020-12-04 17:55:30 +0800

    20201204

--- Elos_Service_V1_0_0/src/main.ts
+++ Elos_Service_V1_0_0/src/main.ts
@@ -4,7 +4,7 @@ declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(80);
+  await app.listen(3090);
   if (module.hot) {
     module.hot.accept();
     module.hot.dispose(() => app.close());

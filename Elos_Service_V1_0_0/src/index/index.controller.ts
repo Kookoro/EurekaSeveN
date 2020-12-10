3bbf9fd348e3c3bfce2d61f5b5ff3b83c8d2d66d
qiulin <710937671@qq.com>
2020-12-10 15:50:30 +0800

    20201210

--- Elos_Service_V1_0_0/src/index/index.controller.ts
+++ Elos_Service_V1_0_0/src/index/index.controller.ts
@@ -1,9 +1,31 @@
 import { Controller, Get } from '@nestjs/common';
 
-@Controller('index')
+interface Message {}
+@Controller('api')
 export class IndexController {
   @Get('user')
   getUserInfo(): String {
     return '<h1>1</h1>';
+  }
+
+  @Get('notice')
+  getMessage(): object[] {
+    return [
+      {
+        id: '000001',
+        text: '《小康人报》第276期领取通知',
+        url: 'xxxxx',
+      },
+      {
+        id: '000001',
+        text: '《小康人报》第277期领取通知',
+        url: 'xxxxx',
+      },
+      {
+        id: '000001',
+        text: '《小康人报》第278期领取通知',
+        url: 'xxxxx',
+      },
+    ];
   }
 }

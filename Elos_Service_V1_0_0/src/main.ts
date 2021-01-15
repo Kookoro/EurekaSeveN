86cc3af04412d0283e2dc7665f88e03ea53fc320
qiulin <710937671@qq.com>
2021-01-15 14:29:00 +0800

    静态资源访问

--- Elos_Service_V1_0_0/src/main.ts
+++ Elos_Service_V1_0_0/src/main.ts
@@ -1,17 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { dirname, join } from 'path';
+
+import { join } from 'path';
 import { AppModule } from './app.module';
+
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
   app.enableCors();
   //静态图片地址
-  app.useStaticAssets('static/images'); //配置静态目录
-  app.useStaticAssets('static/images', {
-    prefix: '/images/',
-  }); //配置虚拟静态目录
+  // app.useStaticAssets(join(__dirname,'..','public'),'static/images'); //配置静态目录
+  app.useStaticAssets('public/images'); //直接访问图片
 
   await app.listen(3090);
   if (module.hot) {

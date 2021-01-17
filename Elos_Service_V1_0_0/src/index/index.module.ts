09cb081a50b404f4915f77ca57cd1aeaeb50a9e3
qiulin <linlinnnn@foxmail.com>
2021-01-17 15:19:23 +0800

    20210117

--- Elos_Service_V1_0_0/src/index/index.module.ts
+++ Elos_Service_V1_0_0/src/index/index.module.ts
@@ -1,8 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Module,HttpModule} from '@nestjs/common';
 import { IndexController } from './index.controller';
 import { IndexService } from './index.service';
 @Module({
-  imports: [],
+  imports: [HttpModule.register({
+    timeout: 5000,
+    maxRedirects: 5,
+  }),],
   controllers: [IndexController],
   providers: [IndexService],
 })
--- Elos_Service_V1_0_0/src/index/index.service.ts
+++ Elos_Service_V1_0_0/src/index/index.service.ts
@@ -1,19 +1,28 @@
 import { Injectable, HttpService } from '@nestjs/common';
-import { DTOAdminInfo } from './index.dto';
+import { Observable } from 'rxjs';
+import { DTOAdminInfo } from './dto/index.dto';
+import { AxiosResponse } from 'axios';
 @Injectable()
 export class IndexService {
-  // constructor(private readonly http: HttpService) {}
+  constructor(private readonly httpService: HttpService) {}
   getAdminInfo(): DTOAdminInfo {
     return {
       adminName: 'TsuBaSa',
     };
   }
-  async getDailyImg() {
-    const http = new HttpService();
-    const url = `https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN`;
-    const response = await http.get(url);
-    return response;
+  // async getDailyImg() {
+  //   const http = new HttpService();
+  //   const url = `https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN`;
+  //   const response = await http.get(url);
+  //   return response;
+  // }
+
+  getDailyImg(): Observable<AxiosResponse<IndexService[]>> {
+    const result = this.httpService.get('https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN')
+    return result;
   }
+    
+
   getUserMsg(): object {
     return {
       obj: '1',
